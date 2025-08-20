a8e4b5b5cf0d3f58a645cac5342d127bcacc07f6
Johan Meyer <y-core@outlook.com>
2025-08-20 07:08:21 +0200

    Phase 1

---
+++ dist/date/index.d.ts
@@ -0,0 +1,1 @@
+export { AUTH_SESSION_TTL, AUTH_TOTP_PERIOD, addTimeToNow, getCurrentTimestamp, isExpired } from './timestamp-utils';
---
+++ dist/logging/index.d.ts
@@ -0,0 +1,2 @@
+export type { BaseLogParams, LogLevel, LogParams } from './@types/logger.types';
+export { logger } from './logger';
