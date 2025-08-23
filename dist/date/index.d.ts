269c2a7c1ef8934ef10c76cd9fa06f2a71b05c7e
Johan Meyer <y-core@outlook.com>
2025-08-23 12:32:01 +0200

    Remove dist

--- dist/date/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { AUTH_SESSION_TTL, AUTH_TOTP_PERIOD, addTimeToNow, getCurrentTimestamp, isExpired } from './timestamp-utils';
--- dist/logging/index.d.ts
+++
@@ -1,2 +0,0 @@
-export type { BaseLogParams, LogLevel, LogParams } from './@types/logger.types';
-export { logger } from './logger';
