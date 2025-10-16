59d8df20996b4f5fe5aeb2e269150a6e9ac23fff
Johan Meyer <y-core@outlook.com>
2025-10-16 15:53:10 +0200

    Update workspace packages to v 2.3.0

---
+++ dist/intent/server/index.d.ts
@@ -0,0 +1,2 @@
+export type { IntentActionConfig, IntentHandler, IntentHandlers, IntentResponse } from '../../@types/intent.types';
+export { getIntent, handleIntent, hasIntent } from './intent-action';
---
+++ dist/result/index.d.ts
@@ -0,0 +1,7 @@
+export type { AppError, FieldErrors, ResponseMeta, Result, ToastConfig } from './@types/result.types';
+export { badRequestError, extractFieldErrors, extractFieldErrors as formErrors, flattenError, forbiddenError, notFoundError, serverError, toAppError, transformError, unauthorizedError, validationError } from './core/error';
+export { andThen, combine, combineObject, err, getData, getError, isError, isOk, map, mapError, ok, orElse, tryCatch, unwrap, unwrapOr } from './core/result';
+export { getClientIP } from './http/headers';
+export { middlewareFailure, middlewarePassthrough } from './http/middleware';
+export { respondError, respondOk } from './http/response';
+export { createValidationMiddleware, validateFormData, validateJsonData, validateQueryParams } from './validation/form';
